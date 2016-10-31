fd540cbdd2ccc226b2db83844bee850c03017724
EWKnudsen <ewknudsen@gmail.com>
2016-10-31 15:04:44 +0100

    project added new project kinect v2 hep hey

---
+++ Assets/KinectDemos/FittingRoomDemo/Scripts/UserBodyBlender.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class UserBodyBlender : MonoBehaviour 
+{
+	[Tooltip("Allowed depth distance between the user and the cloth model.")]
+	[Range(-0.5f, 0.5f)]
+	public float depthThreshold = 0.1f;
+
+	private Material userBlendMat;
+	private KinectManager kinectManager;
+	private long lastDepthFrameTime;
+
+	private Vector2[] color2DepthCoords;
+	private ComputeBuffer color2DepthBuffer;
+
+	private float[] depthImageBufferData;
+	private ComputeBuffer depthImageBuffer;
+
+	private Rect shaderUvRect = new Rect(0, 0, 1, 1);
+	private bool shaderRectInited = false;
+
+	private float depthFactor = 1f;
+
+
+	void Start () 
+	{
+		GetComponent<Camera>().depthTextureMode = DepthTextureMode.Depth;
+
+		kinectManager = KinectManager.Instance;
+
+		if(kinectManager && kinectManager.IsInitialized() &&
+			kinectManager.GetSensorData().sensorIntPlatform == KinectInterop.DepthSensorPlatform.KinectSDKv2)
+		{
+			Shader userBlendShader = Shader.Find("Custom/UserBlendShader");
+			KinectInterop.SensorData sensorData = kinectManager.GetSensorData();
+
+			if(userBlendShader != null && sensorData != null)
+			{
+				userBlendMat = new Material(userBlendShader);
+				
+				userBlendMat.SetFloat("_ColorResX", (float)sensorData.colorImageWidth);
+				userBlendMat.SetFloat("_ColorResY", (float)sensorData.colorImageHeight);
+				userBlendMat.SetFloat("_DepthResX", (float)sensorData.depthImageWidth);
+				userBlendMat.SetFloat("_DepthResY", (float)sensorData.depthImageHeight);
+
+				depthFactor = 1f + Mathf.Sin (Mathf.Abs (kinectManager.sensorAngle) * Mathf.Deg2Rad);
+				userBlendMat.SetFloat("_DepthFactor", depthFactor);
+
+				color2DepthCoords = new Vector2[sensorData.colorImageWidth * sensorData.colorImageHeight];
+
+				color2DepthBuffer = new ComputeBuffer(sensorData.colorImageWidth * sensorData.colorImageHeight, sizeof(float) * 2);
+				userBlendMat.SetBuffer("_DepthCoords", color2DepthBuffer);
+
+				depthImageBufferData = new float[sensorData.depthImage.Length];
+
+				depthImageBuffer = new ComputeBuffer(sensorData.depthImage.Length, sizeof(float));
+				userBlendMat.SetBuffer("_DepthBuffer", depthImageBuffer);
+			}
+		}
+	}
+
+	void OnDestroy()
+	{
+		if(color2DepthBuffer != null)
+		{
+			color2DepthBuffer.Release();
+			color2DepthBuffer = null;
+		}
+		
+		if(depthImageBuffer != null)
+		{
+			depthImageBuffer.Release();
+			depthImageBuffer = null;
+		}
+
+		color2DepthCoords = null;
+		depthImageBufferData = null;
+	}
+
+	void Update()
+	{
+		if (!shaderRectInited) 
+		{
+			PortraitBackground portraitBack = PortraitBackground.Instance;
+			if(portraitBack && portraitBack.IsInitialized())
+			{
+				shaderUvRect = portraitBack.GetShaderUvRect();
+			}
+
+			if (userBlendMat != null) 
+			{
+				userBlendMat.SetFloat("_ColorOfsX", shaderUvRect.x);
+				userBlendMat.SetFloat("_ColorMulX", shaderUvRect.width);
+				userBlendMat.SetFloat("_ColorOfsY", shaderUvRect.y);
+				userBlendMat.SetFloat("_ColorMulY", shaderUvRect.height);
+			}
+
+			shaderRectInited = true;
+		}
+
+		if(kinectManager && kinectManager.IsInitialized())
+		{
+			KinectInterop.SensorData sensorData = kinectManager.GetSensorData();
+			
+			if(sensorData != null && sensorData.depthImage != null && sensorData.colorImageTexture &&
+				userBlendMat != null && lastDepthFrameTime != sensorData.lastDepthFrameTime)
+			{
+				lastDepthFrameTime = sensorData.lastDepthFrameTime;
+				userBlendMat.SetTexture("_ColorTex", sensorData.colorImageTexture);
+
+				if (kinectManager.autoHeightAngle == KinectManager.AutoHeightAngle.AutoUpdate || kinectManager.autoHeightAngle == KinectManager.AutoHeightAngle.AutoUpdateAndShowInfo) 
+				{
+					depthFactor = 1f + Mathf.Sin (Mathf.Abs (kinectManager.sensorAngle) * Mathf.Deg2Rad);
+					userBlendMat.SetFloat("_DepthFactor", depthFactor);
+				}
+
+				if(KinectInterop.MapColorFrameToDepthCoords(sensorData, ref color2DepthCoords))
+				{
+					color2DepthBuffer.SetData(color2DepthCoords);
+				}
+				
+				for (int i = 0; i < sensorData.depthImage.Length; i++)
+				{
+					int depth = sensorData.depthImage[i];
+					depthImageBufferData[i] = (float)depth;
+				}
+				
+				depthImageBuffer.SetData(depthImageBufferData);
+			}
+		}
+	}
+	
+	void OnRenderImage (RenderTexture source, RenderTexture destination)
+	{
+		if(userBlendMat != null)
+		{
+			userBlendMat.SetFloat("_Threshold", depthThreshold);
+			Graphics.Blit(source, destination, userBlendMat);
+		}
+	}
+}
---
+++ Assets/KinectDemos/OverlayDemo/Scripts/PhotoBoothController.cs
@@ -0,0 +1,301 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class PhotoBoothController : MonoBehaviour, KinectGestures.GestureListenerInterface, InteractionListenerInterface
+{
+	[Tooltip("GUI-texture used to display the color camera feed on the scene background.")]
+	public GUITexture backgroundImage;
+
+	[Tooltip("Camera that will be used to render the background.")]
+	public Camera backroundCamera;
+
+	[Tooltip("Camera that will be used to overlay the 3D-objects over the background.")]
+	public Camera foreroundCamera;
+
+	[Tooltip("Index of the player, tracked by this component. 0 means the 1st player, 1 - the 2nd one, 2 - the 3rd one, etc.")]
+	public int playerIndex = 0;
+
+	[Tooltip("Reference to the head joint-overlayer component.")]
+	public JointOverlayer headOverlayer;
+
+	[Tooltip("Reference to the left hand joint-overlayer component.")]
+	public JointOverlayer leftHandOverlayer;
+
+	[Tooltip("Reference to the chest joint-overlayer component.")]
+	public JointOverlayer chestOverlayer;
+
+	[Tooltip("Array of sprite transforms that will be used for head overlays on each step.")]
+	public Transform[] headMasks;
+
+	[Tooltip("Array of sprite transforms that will be used for left hand overlays on each step.")]
+	public Transform[] leftHandMasks;
+
+	[Tooltip("Array of sprite transforms that will be used for chest overlays on each step.")]
+	public Transform[] chestMasks;
+
+	[Tooltip("Array of sprite transforms that will be used for displaying the countdown until image shot.")]
+	public Transform[] countdown;
+
+	[Tooltip("GUI-Text used to display information messages.")]
+	public GUIText infoText;
+
+
+	private int maskCount = 0;
+	private int currentIndex = -1;
+	private int prevIndex = -1;
+
+
+	void Start () 
+	{
+		maskCount = 0;
+
+		if (headMasks != null && headMasks.Length > maskCount)
+			maskCount = headMasks.Length;
+
+		if (leftHandMasks != null && leftHandMasks.Length > maskCount)
+			maskCount = leftHandMasks.Length;
+
+		if (chestMasks != null && chestMasks.Length > maskCount)
+			maskCount = chestMasks.Length;
+	}
+	
+	void Update () 
+	{
+		KinectManager manager = KinectManager.Instance;
+
+		if (manager && manager.IsInitialized ()) 
+		{
+			if (backgroundImage && (backgroundImage.texture == null)) 
+			{
+				backgroundImage.texture = manager.GetUsersClrTex ();
+			}
+		}
+
+		if (currentIndex != prevIndex) 
+		{
+			prevIndex = currentIndex;
+
+			if (headOverlayer && headMasks != null) 
+			{
+				if (headOverlayer.overlayObject)
+					headOverlayer.overlayObject.gameObject.SetActive (false);
+				
+				headOverlayer.overlayObject = currentIndex >= 0 && currentIndex < headMasks.Length ? headMasks [currentIndex] : null;
+				headOverlayer.playerIndex = playerIndex;
+
+				if (headOverlayer.overlayObject)
+					headOverlayer.overlayObject.gameObject.SetActive (true);
+			}
+
+			if (leftHandOverlayer && leftHandMasks != null) 
+			{
+				if (leftHandOverlayer.overlayObject)
+					leftHandOverlayer.overlayObject.gameObject.SetActive (false);
+
+				leftHandOverlayer.overlayObject = currentIndex >= 0 && currentIndex < leftHandMasks.Length ? leftHandMasks [currentIndex] : null;
+				leftHandOverlayer.playerIndex = playerIndex;
+
+				if (leftHandOverlayer.overlayObject)
+					leftHandOverlayer.overlayObject.gameObject.SetActive (true);
+			}
+
+			if (chestOverlayer && chestMasks != null) 
+			{
+				if (chestOverlayer.overlayObject)
+					chestOverlayer.overlayObject.gameObject.SetActive (false);
+
+				chestOverlayer.overlayObject = currentIndex >= 0 && currentIndex < chestMasks.Length ? chestMasks [currentIndex] : null;
+				chestOverlayer.playerIndex = playerIndex;
+
+				if (chestOverlayer.overlayObject)
+					chestOverlayer.overlayObject.gameObject.SetActive (true);
+			}
+		}
+	}
+
+
+	// GestureListenerInterface
+
+	public void UserDetected(long userId, int userIndex)
+	{
+		KinectManager manager = KinectManager.Instance;
+		if(!manager || (userIndex != playerIndex))
+			return;
+
+		manager.DetectGesture(userId, KinectGestures.Gestures.SwipeLeft);
+		manager.DetectGesture(userId, KinectGestures.Gestures.SwipeRight);
+
+		currentIndex = 0;
+
+		if (infoText) 
+		{
+			infoText.text = "Swipe left or right to change props. Make hand grip to take photo.";
+		}
+	}
+
+	public void UserLost(long userId, int userIndex)
+	{
+		if(userIndex != playerIndex)
+			return;
+
+		currentIndex = -1;
+	}
+
+	public void GestureInProgress(long userId, int userIndex, KinectGestures.Gestures gesture, float progress, KinectInterop.JointType joint, Vector3 screenPos)
+	{
+		// nothing to do here
+	}
+
+	public bool GestureCompleted(long userId, int userIndex, KinectGestures.Gestures gesture, KinectInterop.JointType joint, Vector3 screenPos)
+	{
+		if(userIndex != playerIndex)
+			return false;
+
+		switch (gesture)
+		{
+		case KinectGestures.Gestures.SwipeLeft:
+			currentIndex++;
+			if (currentIndex >= maskCount)
+				currentIndex = 0;
+			break;
+
+		case KinectGestures.Gestures.SwipeRight:
+			currentIndex--;
+			if (currentIndex < 0)
+				currentIndex = maskCount - 1;
+			break;
+		}
+
+		return true;
+	}
+
+	public bool GestureCancelled(long userId, int userIndex, KinectGestures.Gestures gesture, KinectInterop.JointType joint)
+	{
+		if(userIndex != playerIndex)
+			return false;
+
+		return true;
+	}
+
+
+	// InteractionListenerInterface
+
+	public void HandGripDetected(long userId, int userIndex, bool isRightHand, bool isHandInteracting, Vector3 handScreenPos)
+	{
+		if (userIndex != playerIndex)
+			return;
+
+		if (isRightHand && handScreenPos.y >= 0.5f) 
+		{
+			if (infoText) 
+			{
+				infoText.text = "Hand grip detected.";
+			}
+
+			StartCoroutine(CountdownAndTakePicture());
+		}
+	}
+
+	public void HandReleaseDetected(long userId, int userIndex, bool isRightHand, bool isHandInteracting, Vector3 handScreenPos)
+	{
+		if (userIndex != playerIndex)
+			return;
+
+		// nothing to do here
+	}
+
+	public bool HandClickDetected(long userId, int userIndex, bool isRightHand, Vector3 handScreenPos)
+	{
+		if (userIndex != playerIndex)
+			return false;
+
+		return true;
+	}
+
+
+	// counts from 3 and takes a picture
+	private IEnumerator CountdownAndTakePicture()
+	{
+		if (countdown.Length > 0) 
+		{
+			for(int i = 0; i < countdown.Length; i++)
+			{
+				if (countdown [i])
+					countdown [i].gameObject.SetActive(true);
+				
+				yield return new WaitForSeconds(1.0f);
+
+				if (countdown [i])
+					countdown [i].gameObject.SetActive(false);
+			}
+		}
+
+		TakePicture();
+		yield return null;
+	}
+
+
+	// saves the screen image as png picture
+	private string TakePicture()
+	{
+		if (backroundCamera && foreroundCamera) 
+		{
+			int resWidth = Screen.width;
+			int resHeight = Screen.height;
+
+			Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false); //Create new texture
+			RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
+
+			// hide the info-text, if any
+			if (infoText) 
+			{
+				infoText.text = string.Empty;
+			}
+
+			// render background and foreground cameras
+			backroundCamera.targetTexture = rt;
+			backroundCamera.Render();
+
+			foreroundCamera.targetTexture = rt;
+			foreroundCamera.Render();
+
+			// get the screenshot
+			RenderTexture.active = rt;
+			screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+
+			// clean-up
+			backroundCamera.targetTexture = null;
+			foreroundCamera.targetTexture = null;
+
+			RenderTexture.active = null;
+			Destroy(rt);
+
+			byte[] btScreenShot = screenShot.EncodeToJPG();
+			Destroy(screenShot);
+
+#if !UNITY_WSA
+			// save the screenshot as jpeg file
+			string sDirName = Application.dataPath + "/Screenshots";
+			if (!Directory.Exists(sDirName))
+				Directory.CreateDirectory (sDirName);
+			
+			string sFileName = sDirName + "/" + string.Format ("{0:F0}", Time.realtimeSinceStartup * 10f) + ".jpg";
+			File.WriteAllBytes(sFileName, btScreenShot);
+
+			if (infoText) 
+			{
+				infoText.text = "Saved to: " + sFileName;
+			}
+				
+			// open file
+			System.Diagnostics.Process.Start(sFileName);
+
+			return sFileName;
+#endif
+		}
+
+		return string.Empty;
+	}
+
+}
