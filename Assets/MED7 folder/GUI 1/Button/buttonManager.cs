a648b8629b9cbfa00d5f9d689d729fe36a203c03
Mads Juel Nielsen <MadsJuel@h228.aau1x-3.klient.sydhavn2.site.aau.dk>
2016-11-01 14:40:48 +0100

    Added Gui and Scenes, HEP

---
+++ Assets/MED7 folder/GUI 1/Button/buttonManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class buttonManager : MonoBehaviour
+{
+	public void startBtn(string startScene)
+	{
+		SceneManager.LoadScene (startScene);
+	}
+
+	public void infoBtn(string infoScene)
+	{
+		SceneManager.LoadScene (infoScene);
+	}
+
+	public void backBtn1(string menuScene)
+	{
+		SceneManager.LoadScene (menuScene);
+	}
+
+	public void exitBtn()
+	{
+		Application.Quit ();
+	}
+}
+
