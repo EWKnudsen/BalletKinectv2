bf4b0d450d0bee66bfee55d7e1a477cb899d5fb8
EWKnudsen <ewknudsen@gmail.com>
2016-11-08 14:12:28 +0100

    audio feedback

---
+++ Assets/MED7 folder/Scripts/CMCfilterRESETTER.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class CMCfilterRESETTER : MonoBehaviour
+{
+    public AudioMixer theMixer;
+
+    void Start ()
+    {
+        theMixer.SetFloat("PitchShift", 1);
+        theMixer.SetFloat("CutOffFreqHP", 10);
+        theMixer.SetFloat("CutOffFreqLP", 22000);
+    }
+}
