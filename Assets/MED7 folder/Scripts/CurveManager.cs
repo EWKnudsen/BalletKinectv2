8a9bdbb60c3f1eb4f64c855cf9d1c6988f6299e1
Malthdk <malte@holledig.dk>
2016-11-16 15:44:40 +0100

    Commit

--- Assets/MED7 folder/Scripts/CurveManager.cs
+++ Assets/MED7 folder/Scripts/CurveManager.cs
@@ -4,7 +4,10 @@ using System.Collections.Generic;
 
 public class CurveManager : MonoBehaviour {
 
-	public CMCTorsoNotchAtten script;
+	public CMCPelvisHP pelvisHp;
+	public CMCPelvisNotchAtten pelvisNotch;
+	public CMCTorsoHP torsoHp;
+	public CMCTorsoNotchAtten torsoNotch; 
 	public float score;
 	private ParticleSystem ps;
 	private Vector3 pos;
@@ -12,7 +15,10 @@ public class CurveManager : MonoBehaviour {
 	private float amount;
 
 	void Start () {
-		script = GameObject.Find ("FilterController").GetComponent<CMCTorsoNotchAtten> ();
+		pelvisHp = GameObject.Find ("FilterController").GetComponent<CMCPelvisHP>();
+		pelvisNotch = GameObject.Find ("FilterController").GetComponent<CMCPelvisNotchAtten>();
+		torsoHp = GameObject.Find ("FilterController").GetComponent<CMCTorsoHP>();
+		torsoNotch = GameObject.Find ("FilterController").GetComponent<CMCTorsoNotchAtten>(); 
 		ps = transform.gameObject.GetComponent<ParticleSystem> ();
 		pos = transform.position;
 	}
@@ -24,7 +30,15 @@ public class CurveManager : MonoBehaviour {
 
 	void CalculatePosition() {
 		float yMin = 0.55f;
-		score = script.score;
+		if (pelvisHp.enabled) {
+			score = pelvisHp.score;
+		} else if (pelvisNotch.enabled) {
+			score = pelvisNotch.score;
+		} else if (torsoHp.enabled) {
+			score = torsoHp.score;
+		} else if (torsoNotch.enabled) {
+			score = torsoNotch.score;
+		}
 		ratio = score/100;
 		amount = 0.20f*ratio;
 		pos.y = yMin + pos.y*amount;
