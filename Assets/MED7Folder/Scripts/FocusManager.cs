655b2e07cfe9ce4798b25d243cd906226d2c114e
EWKnudsen <ewknudsen@gmail.com>
2016-11-25 14:16:14 +0100

    hpper

---
+++ Assets/MED7Folder/Scripts/FocusManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class FocusManager : MonoBehaviour {
+
+	bool sceneChanged = false;
+	public GameObject prefab;
+	public int focusIndex;
+
+	void Awake() {
+		DontDestroyOnLoad(transform.gameObject);
+	}
+
+	void Start () {
+	
+	}
+
+	void Update () {
+		if (SceneManager.GetActiveScene().name == "MainScene" && !sceneChanged) {
+
+			GameObject controller = GameObject.Find("KinectControllerCursor");
+
+			if (controller != null) {
+				Destroy(controller);
+			}
+				
+			CMCCombinedTorAndPelHP postureScript = GameObject.Find ("FilterController").GetComponent<CMCCombinedTorAndPelHP>();
+
+			// CHANGE THIS FOR HANDS SCRIPT:
+			CMCTorsoHP symmetryScript = GameObject.Find ("FilterController").GetComponent<CMCTorsoHP>();
+
+			postureScript.enabled = false;
+			symmetryScript.enabled = false;
+
+			if (focusIndex == 0) {
+				postureScript.enabled = true;	
+			}
+			if (focusIndex == 1) {
+				symmetryScript.enabled = true;	
+			}
+
+			sceneChanged = true;
+		}
+
+		if (SceneManager.GetActiveScene().name == "GameOver" && sceneChanged) {
+
+			Instantiate(prefab);
+
+			sceneChanged = false;
+
+		}
+	}
+
+	public void ChooseFocus (int i) {
+		focusIndex = i;
+	}
+}
---
+++ Assets/MED7Folder/Scripts/PlayVideo.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+
+public class PlayVideo : MonoBehaviour {
+
+	public MovieTexture movie;
+	public float videoDuration;
+	private bool moviePlayed;
+	public float warmUpTime = 3.2f;
+	private int i;
+
+	// Use this for initialization
+	void Awake() {
+		videoDuration = movie.duration;
+	}
+
+	void Start () {
+		movie.Play();
+		i = 0;
+		StartCoroutine("MovieEnded");
+		StartCoroutine("WarmUpSession");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// This is in order to freeze the first frame while warm-up session is ongoing
+		i++;
+		if (i==2) {
+			movie.Pause();
+		}
+	}
+
+	void PlayMovie() {
+		movie.Play();
+	}
+
+	private IEnumerator WarmUpSession() {
+		yield return new WaitForSeconds(warmUpTime);
+		movie.Play();
+	}
+
+	private IEnumerator MovieEnded() {
+		yield return new WaitForSeconds(videoDuration+warmUpTime);
+		SceneManager.LoadScene ("GameOver");
+	}
+}
