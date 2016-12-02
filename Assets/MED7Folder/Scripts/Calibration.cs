93822dc7afe36bc2511905d27c2e9c0a58cb8e40
Malte Hølledig <malte@holledig.dk>
2016-12-02 01:23:55 +0100

    Made calibration scene, average calculator and script that can be transfered between scenes. Also made adjustments to other scenes.

---
+++ Assets/MED7Folder/Scripts/Calibration.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Calibration : MonoBehaviour {
+
+	[HideInInspector]
+	public UIManager uiManager;
+	[HideInInspector]
+	public CubemanController cubeman;
+
+	public Vector3 calibratedHip, calibratedShoulder, calibratedNeck;
+	int counter;
+
+	void Awake() {
+		DontDestroyOnLoad(transform.gameObject);
+	}
+
+	void Start () {
+		uiManager = GameObject.Find ("UIManager").GetComponent<UIManager> ();
+		cubeman = GameObject.Find ("Cubeman").GetComponent<CubemanController> ();
+	}
+	
+
+	void Update () {
+		if (uiManager.calibrating) {
+			StartCoroutine("CalculateAverage");
+		} 
+		if (uiManager.finished) {
+			calibratedHip = calibratedHip/counter;
+			calibratedShoulder = calibratedShoulder/counter;
+			calibratedNeck = calibratedNeck/counter;
+			StopCoroutine("CalculateAverage");
+			uiManager.finished = false;
+		}
+	}
+
+	IEnumerator CalculateAverage() {
+		calibratedHip += cubeman.Hip_Center.transform.position;
+		calibratedShoulder += cubeman.shoulderCenterPos;
+		calibratedNeck += cubeman.Neck.transform.position;
+
+		counter++;
+		yield return new WaitForSeconds(0.2f);
+		StartCoroutine("CalculateAverage");
+	}
+}
--- Assets/MED7Folder/Scripts/UIManager.cs
+++ Assets/MED7Folder/Scripts/UIManager.cs
@@ -6,25 +6,73 @@ using System.Collections;
 public class UIManager : MonoBehaviour {
 
 	//public Text uiText;
+	[HideInInspector]
 	public PlayVideo videoScript;
+	[HideInInspector]
 	public Image timer;
-	float clipDuration;
+	[HideInInspector]
+	public GameObject background;
+	[HideInInspector]
+	public Text instructions;
+	public bool calibrationTimer;
+	public bool calibrating;
+	[HideInInspector]
+	public bool finished;
+	float duration;
 	float timeLeft;
+	float elapsedTime;
 
 	// Use this for initialization
 	void Start () {
-		//uiText = FindObjectOfType<Text>();
-		videoScript = GameObject.Find ("Video").GetComponent<PlayVideo> ();
 		timer = GameObject.Find ("Timer").GetComponent<Image> ();
-		clipDuration = videoScript.videoDuration;
+		background = GameObject.Find ("Background");
+		if (!calibrationTimer) {
+			videoScript = GameObject.Find ("Video").GetComponent<PlayVideo> ();
+			duration = videoScript.videoDuration;
+		} else {
+			background.gameObject.SetActive(false);
+			instructions = GameObject.Find ("Instructions").transform.GetChild(0).GetComponent<Text>();
+			duration = 10f;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (timer != null) {
-			timeLeft = Time.timeSinceLevelLoad / clipDuration;
+		// For calculating time that can be set
+		elapsedTime += Time.deltaTime;
+
+		// For MainScene to display timer
+		if (timer != null && videoScript != null) {
+			timeLeft = Time.timeSinceLevelLoad / duration;
 			timer.fillAmount = timeLeft;
 		}
+
+		// For checking when a key is down - only used in calibration scene
+		if (calibrationTimer && Input.anyKeyDown) {
+			calibrating = true;
+			elapsedTime = 0f;
+			background.gameObject.SetActive(true);
+			GameObject.Find ("Instructions").transform.GetChild(1).gameObject.SetActive(false);
+			instructions.text = "CALIBRATING...\n Please hold your position";
+		}
+
+		// When calibrating fill up the timer image. When it is filled change text and change scene
+		if (calibrating) {
+			timeLeft = elapsedTime / duration;
+			timer.fillAmount = timeLeft;
+
+			if (timer.fillAmount == 1f) {
+				StartCoroutine("Finished");
+			}
+		}
+	}
+
+	IEnumerator Finished() {
+		instructions.text = "FINISHED CALIBRATING";
+		yield return new WaitForSeconds(1.5f);
+		calibrating = false;
+		finished = true;
+		SceneManager.LoadScene("PlieDetails");
 	}
 
 }
