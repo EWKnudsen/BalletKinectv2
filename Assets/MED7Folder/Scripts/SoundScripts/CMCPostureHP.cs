11fb655e4799fe988aa2c548758d35bb1216eb36
EWKnudsen <ewknudsen@gmail.com>
2016-12-05 01:17:54 +0100

    added a new posture script that will in the end have all the posture to highpass functions, including calibration

---
+++ Assets/MED7Folder/Scripts/SoundScripts/CMCPostureHP.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+public class CMCPostureHP : MonoBehaviour
+{
+    public AudioMixer theMixer;
+    public CubemanController CMCScript;
+    public PlayVideo video;
+    StreamWriter writer;
+    public float torso_score, pelvis_score, score;
+
+    double T_distHipShoulder, T_distShoulderNeck, T_totalDist;
+    double P_totalRot;
+    double C_totalComb, C_HPfilterVal;
+
+    double minFreq = 20;
+    double maxFreq = 22000;
+    double HPfilterVal;
+
+    static double T_minDist = 0.011; //0.0085f
+    static double T_maxDist = 0.187; //0.125f
+
+    static double P_minRot = 0.063;
+    static double P_maxRot = 0.31;
+
+
+    void Start()
+    {
+        CMCScript = GameObject.Find("Cubeman").GetComponent<CubemanController>();
+        video = GameObject.Find("Video").GetComponent<PlayVideo>();
+        writer = new StreamWriter("Assets/MED7Folder/Txtfiles/MPCTest.txt", true);
+        writer.WriteLine("New score data:");
+
+        theMixer.SetFloat("Torso_Attentuation", 0);
+    }
+
+
+    void Update()
+    {
+        if (CMCScript != null)
+        {
+            if (CMCScript.hasValues)
+            {
+                
+                T_distHipShoulder  = CalXZdist(CMCScript.shoulderCenterPos, CMCScript.hipCenterPos);
+                T_distShoulderNeck = CalXZdist(CMCScript.shoulderCenterPos, CMCScript.neckPos);
+
+                T_totalDist = T_distHipShoulder + T_distShoulderNeck;
+                P_totalRot = Math.Abs(CMCScript.hipCenterRot.x) + Math.Abs(CMCScript.hipCenterRot.y) + Math.Abs(CMCScript.hipCenterRot.z);
+
+                C_totalComb = (T_totalDist + P_totalRot) / 2;
+                
+                HPfilterVal = HighPassScaling(C_totalComb, (T_minDist + P_minRot), (T_maxDist + P_maxRot) , minFreq, maxFreq);
+                theMixer.SetFloat("Torso_CutOffFreqHP", (float)HPfilterVal);
+
+
+                UpdateScores(T_totalDist, T_minDist, T_maxDist, P_totalRot, P_minRot, P_maxRot);
+
+
+                StreamWriteValues(T_totalDist, P_totalRot);
+
+
+                //Debug.Log("T: " + T_totalDist + "     R: " + P_totalRot);
+            }
+        }
+    }
+
+    private double CalXZdist(Vector3 vecA, Vector3 vecB)
+    {
+        return Math.Sqrt(Math.Pow(vecB.x - vecA.x, 2) + Math.Pow(vecB.z - vecA.z, 2));
+    }
+
+    private double HighPassScaling(double unscaledVal, double minOld, double maxOld, double minNew, double maxNew) 
+    {
+        if (unscaledVal < minOld)
+            unscaledVal = minOld;
+        if (unscaledVal > maxOld)
+            unscaledVal = maxOld;
+        
+        return minNew * Math.Pow((Math.Pow((maxNew / minNew), (1 / (maxOld - minOld)))), (maxOld - unscaledVal));
+    }
+
+    protected double LinearScaling(double unscaledVal, double minOld, double maxOld, double minNew, double maxNew)
+    {
+        if (unscaledVal < minOld)
+            unscaledVal = minOld;
+        if (unscaledVal > maxOld)
+            unscaledVal = maxOld;
+        
+        return (maxNew - minNew) * (unscaledVal - minOld) / (maxOld - minOld) + minNew;
+    }
+
+    void UpdateScores(double T_val, double T_min, double T_max, double P_val, double P_min, double P_max)
+    {
+        torso_score = 100 - (float)LinearScaling(T_val, 0, 100, T_min, T_max);
+        pelvis_score = 100 - (float)LinearScaling(P_val, 0, 100, P_min, P_max);
+        score = (torso_score + pelvis_score) / 2;
+    }
+
+    void StreamWriteValues(double firstVal, double secondVal)
+    {
+        if (writer.BaseStream != null)
+            writer.WriteLine(firstVal + ", " + secondVal);
+
+        if (Time.time > video.movie.duration - 0.5f)
+            writer.Close();
+    }
+
+}
